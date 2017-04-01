f5f76175165af3bc2ccc2d781afe39c15dd79a41
Pompeu <itacir@hotmail.com>
2017-04-01 20:59:10 -0300

    project init

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/users', users);
+
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  res
+    .status(err.status)
+    .json({error: 'not found'});
+
+});
+
+app.use(function(err, req, res) {
+  const message = err.message;
+  const error = req.app.get('env') === 'development' ? err : {};
+  res
+    .status(err.status || 500)
+    .json({error,  message});
+});
+
+module.exports = app;
